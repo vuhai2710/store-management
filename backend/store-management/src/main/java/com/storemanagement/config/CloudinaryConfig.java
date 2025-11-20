1ad476632d9c342aaf1be8249569f9459f894618
vuhai_2710 <vuhai271004@gmail.com>
2025-11-20 16:55:56 +0700

    Refactor CloudinaryConfig to use properties

--- backend/store-management/src/main/java/com/storemanagement/config/CloudinaryConfig.java
+++ backend/store-management/src/main/java/com/storemanagement/config/CloudinaryConfig.java
@@ -1,20 +1,30 @@
-package com.storemanagement.config; // Nhớ sửa package cho đúng
+package com.storemanagement.config;
 
 import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.HashMap;
+
 import java.util.Map;
 
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${cloudinary.cloud-name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api-key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api-secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
-        Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", "dpmptlswe"); // Thay vào đây
-        config.put("api_key", "593827271151324");       // Thay vào đây
-        config.put("api_secret", "QyB_JC8u02WFwwixGMqFKdkz0uI"); // Thay vào đây
-        return new Cloudinary(config);
+        return new Cloudinary(Map.of(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
+        ));
     }
 }
